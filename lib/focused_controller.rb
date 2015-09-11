60136d483459442571d2086234c6d657f1c4d766
Hammer <john@musicglue.com>
2015-09-11 13:46:02 +0100

    remove route mapper, it is not our friend

--- lib/focused_controller.rb
+++ lib/focused_controller.rb
@@ -5,5 +5,4 @@ end
 require 'focused_controller/version'
 require 'focused_controller/route'
 require 'focused_controller/mixin'
-require 'focused_controller/route_mapper'
 require 'focused_controller/railtie'
