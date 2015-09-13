557391bdb571a162d0351cde14f6709fd1299e0c
Hammer <john@musicglue.com>
2015-09-13 16:05:57 +0100

    fix route generation and utilise controller_reference when loading controllers

--- lib/focused_controller/adapters/rails_42.rb
+++ lib/focused_controller/adapters/rails_42.rb
@@ -3,8 +3,8 @@ FocusedController.scope_arg_index = 0
 class ActionDispatch::Routing::RouteSet::Dispatcher
   def controller_reference(controller_param)
     @controller_class_names[controller_param] ||= begin
-      if controller_param =~ /\A(.+)_controller\/(.+)\z/ # focused_controller route
-         controller_param.camelize
+      if controller_param =~ /\A(.+)_fc\/(.+)\z/ # focused_controller route
+         controller_param.chomp('_fc').camelize
       else
         "#{controller_param.camelize}Controller" # Rails default
       end
--- lib/focused_controller/route.rb
+++ lib/focused_controller/route.rb
@@ -1,15 +1,16 @@
-require "action_dispatch/routing"
+require 'action_dispatch/routing'
 
 module FocusedController
   class Route < ActionDispatch::Routing::RouteSet::Dispatcher
     attr_reader :name
 
-    def initialize(name)
-      @name = name
+    def initialize(defaults)
+      @name = defaults[:name]
+      super
     end
 
     def call(env)
-      name.constantize.call(env)
+      controller_reference(name).call(env)
     end
 
     alias to_s name
