3322f15663b7b2d864a34567324c84f606933daa
Tyler J. Kuhn <tyler@wargoats.com>
2017-01-20 13:29:00 -0800

    Update route_mapper.rb

--- lib/focused_controller/route_mapper.rb
+++ lib/focused_controller/route_mapper.rb
@@ -55,19 +55,19 @@ module FocusedController
       @scope[:focused_controller_routes]
     end
 
-    def add_route(action, options)
-      options = FocusedController::RouteMapper.new(@scope, { action: action }.merge(options)).options
+    def add_route(action, controller, options, _path, to, via, formatted, anchor, options_constriants)
+      new_options = FocusedController::RouteMapper.new(@scope, { action: action }.merge(options)).options
       if focused_controller_enabled?
         super(
           action,
-          options[:controller],
+          new_options[:controller],
           options,
-          options[:path],
-          options[:to],
-          options[:via],
-          options[:formatted],
-          options[:anchor],
-          options[:options_constraints]
+          path,
+          to,
+          via,
+          formatted,
+          anchor,
+          options_constraints
         )
       else
         super
